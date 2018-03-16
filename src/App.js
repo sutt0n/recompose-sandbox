50599a472e4b69e0c0d8e48b679ca9258445f7aa
Joseph Sutton <jsutton@flexion.us>
2018-03-16 17:00:02 -0500

    added recomposed HOC

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends React.Component {
           </form>
         </div>
 
-        <DataTable
+        <DataTableNew
           columns={[
             { name: "author", label: "Author" },
             { name: "title", label: "Title" }
